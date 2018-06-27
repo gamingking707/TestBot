a71acd6b2101cbefcf4642708c411de19dae3497
gamingking707 <40575900+gamingking707@users.noreply.github.com>
2018-06-26 22:45:40 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -121,6 +121,9 @@ async def on_message(message):
         await client.send_message(message.channel, "Hello "+args+", I'm Dad!")
     elif message.content.lower() == prefix+"ping":
         await client.send_message(message.channel, "Pong! <@"+message.author.id+">")
+    elif message.content.lower().startswith(prefix+"save"):
+        args = message.content.split()
+        os.environ[args[1]] = args[2]
 @client.event
 async def on_message_delete(message):
     await client.send_message(client.get_channel("441431242546872320"), "The message:\n'"+message.content+"'\nby <@"+message.author.id+"> has been deleted")
